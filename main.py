80ef0d9f07270f70f45a8a75a8bb8f75c4c332c1
Artyom Uschenko <net775@yandex.ru>
2024-11-20 19:33:42 +0300

    Реализованна функция по неправильной веденной команде.

--- main.py
+++ main.py
@@ -23,7 +23,11 @@ async def start_command(message: types.Message):
 async def on_startup(_):
     print('Бот был успешно перезапущен!')
 
-
+# Функция эхо, повторяет все сообщения пользователя
+@dp.message_handler()
+async def echo_message(message: types.Message):
+    await message.answer(f"Привет, {message.chat.first_name}!\n\nЕсли нужна помощь, то введи команду /help")
+    print(message)
 
 if __name__ == '__main__':
     executor.start_polling(dp, on_startup=on_startup)
