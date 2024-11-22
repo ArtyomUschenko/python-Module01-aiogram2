6ae441be885530d8a234678ad3c6f4fa855d11b7
Artyom Uschenko <net775@yandex.ru>
2024-11-22 13:56:48 +0300

    Добавлена и стилизованна через markdown раздел help

--- main.py
+++ main.py
@@ -1,18 +1,44 @@
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.utils.markdown import bold,italic,link
 import api_token
 
 bot = Bot(api_token.TOKEN) # токен
 dp = Dispatcher(bot) # диспетчер
 
-HELP_TEXT   = '''
-/help - помощь
-/start - запуск бота
+
+
+# Вариант с поддержкой HTML
+# --------------------------------------------------
+# HELP_TEXT   = '''
+# /help - <b> помощь </b>
+# /start - <i> запуск бота </i>
+# '''
+#
+# @dp.message_handler(commands=['help']) # команда /help
+# async def help_command(message: types.Message):
+#     await message.answer(HELP_TEXT, parse_mode="HTML")  #parse_mode="HTML" - вывод в HTML
+
+
+# Вариант с поддержкой markdown
+# --------------------------------------------------
+HELP_TEXT   = f'''
+/help - {bold('помощь')}
+/start - {italic('запуск бота')}
+ {link("ГИС ГосЭДО",'https://gosedo.ru/')} - {italic('официальный сайт')}
+
 '''
+
+@dp.message_handler(commands=['help']) # команда /help
+async def help_command(message: types.Message):
+    await message.answer(HELP_TEXT, parse_mode="markdown")  #parse_mode="markdown" - вывод в markdown
+
+
 
 
 @dp.message_handler(commands=['help']) # команда /help
 async def help_command(message: types.Message):
-    await message.answer(HELP_TEXT)
+    await message.answer(HELP_TEXT, parse_mode="HTML")  #parse_mode="HTML" - вывод в HTML
+
 
 @dp.message_handler(commands=['start']) # команда /start
 async def start_command(message: types.Message):
@@ -26,10 +52,11 @@ async def on_startup(_):
 # Функция эхо, повторяет все сообщения пользователя
 @dp.message_handler()
 async def echo_message(message: types.Message):
-    await message.answer(f"Привет, {message.chat.first_name}!\n\nЕсли нужна помощь, то введи команду /help")
+    await message.answer(f"Привет, {message.from_user.first_name}!\n\nЕсли нужна помощь, то введи команду /help")
     print(message)
 
 if __name__ == '__main__':
     executor.start_polling(dp, on_startup=on_startup)
 
 
+# 4
