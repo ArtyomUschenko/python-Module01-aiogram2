80b72f4287292a906091cd60f6abe711bdd42163
Artyom Uschenko <net775@yandex.ru>
2024-11-28 12:09:42 +0300

    В код добавлены Middlewars для отслеживания апдейтов

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from distutils.command.install_lib import install_lib
-
+from aiogram.dispatcher.middlewares import BaseMiddleware
 from Tools.demo.spreadsheet import rjust
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.utils.markdown import bold,italic,link
 from aiogram.types import InputFile, MediaGroup, ContentType, ChatActions, ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 import logging
+
 
 from random import randint
 from aiohttp.hdrs import CONTENT_RANGE
@@ -63,6 +64,16 @@ keyBoard.add(btn1, btn2, btn3, btn4)
 @dp.errors_handler()
 async def error_handler(update: types.Update, exception: Exception):
     logging.error(f"Ошибка: {update}: {exception}")
+
+
+class MyMiddleware(BaseMiddleware):
+    async def on_pre_process_update(self, update: types.Update, data: dict):
+        print(f"Сообщение перед апдейтом")
+        print(update)
+
+
+    async def on_process_update(self, update: types.Update, data: dict):
+        print(f"Сообщение во время обработки апдейта")
 
 
 @dp.message_handler(commands=['help']) # команда /help
@@ -202,6 +213,7 @@ async def unknown_message(message: types.Message):
 
 
 if __name__ == '__main__':
+    dp.middleware.setup(MyMiddleware())
     executor.start_polling(dp, on_startup=on_startup)
 
 
